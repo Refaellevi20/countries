13c04b4b08d7d2a261fe6e962eaad21220d9ddb9
Refael Levi <refael14levi@gmail.com>
2024-10-06 14:35:32 +0300

    fixed bug

--- controlller.js
+++ controlller.js
@@ -3,7 +3,7 @@
 
 function onInit() {
 
-    document.querySelector('fetch-btn').addEventListener('click', fetchCountryData)
+    document.querySelector('.fetch-btn').addEventListener('click', fetchCountry)
 }
 
 function flashMsg(msg) {
@@ -13,10 +13,26 @@ function flashMsg(msg) {
     setTimeout(() => el.classList.remove('open'), 3000)
 }
 
-function fetchCountryData() {
+function fetchCountry() {
 
-        const  country = document.querySelector('#search').value
-        if (!country) return flashMsg('Please enter a country name!')   
-            
-            ax
-        }
+    const country = document.querySelector('.country-input').value
+    if (!country) return flashMsg('Please enter a country name!')
+
+    axios.get(`https://restcountries.com/v3.1/name/${country}`)
+        .then(response => {
+            const countryData = response.data[0]
+            renderCountryInformation(countryData)
+        })
+        .catch(() => flashMsg('Country not found!'))
+}
+
+
+function renderCountryInformation(countryData) {
+    const countryInformation = `
+    name: ${countryData.common}
+    Population: ${countryData.Population}
+    Flag: ${countryData.flag}
+    Region: ${countryData.region}
+`
+    document.querySelector('country-info').innerHTML = countryInformation
+}
